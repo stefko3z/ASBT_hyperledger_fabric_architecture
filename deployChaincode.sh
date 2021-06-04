c0a709cd762d6b852b1d24ae9bb996063d014827
Stefan Vladov <svladov.dev@gmail.com>
2021-06-04 13:51:53 +0200

    init commit with functioning network

---
+++ deployChaincode.sh
@@ -0,0 +1,226 @@
+export CORE_PEER_TLS_ENABLED=true
+export ORDERER_CA=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export PEER0_PRODUCER1_CA=${PWD}/artifacts/channel/crypto-config/peerOrganizations/producer1.example.com/peers/peer0.producer1.example.com/tls/ca.crt
+export PEER0_PRODUCER2_CA=${PWD}/artifacts/channel/crypto-config/peerOrganizations/producer2.example.com/peers/peer0.producer2.example.com/tls/ca.crt
+export FABRIC_CFG_PATH=${PWD}/artifacts/channel/config/
+
+export PRIVATE_DATA_CONFIG=${PWD}/artifacts/private-data/collections_config.json
+
+export CHANNEL_NAME=mychannel
+
+# setGlobalsForOrderer(){
+#     export CORE_PEER_LOCALMSPID="OrdererMSP"
+#     export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+#     export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/example.com/users/Admin@example.com/msp
+    
+# }
+
+setGlobalsForProducer1Peer0(){
+    export CORE_PEER_LOCALMSPID="Producer1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_PRODUCER1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/producer1.example.com/users/Admin@producer1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+}
+
+setGlobalsForProducer1Peer1(){
+    export CORE_PEER_LOCALMSPID="Producer1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_PRODUCER1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/producer1.example.com/users/Admin@producer1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:8051
+    
+}
+
+setGlobalsForProducer2Peer0(){
+    export CORE_PEER_LOCALMSPID="Producer2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_PRODUCER2_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/producer2.example.com/users/Admin@producer2.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:9051
+    
+}
+
+setGlobalsForProducer2Peer1(){
+    export CORE_PEER_LOCALMSPID="Producer2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_PRODUCER2_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/producer2.example.com/users/Admin@producer2.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:10051
+    
+}
+
+presetup() {
+    echo Vendoring Go dependencies ...
+    pushd ./artifacts/src/github.com/fabcar/go
+    GO111MODULE=on go mod vendor
+    popd
+    echo Finished vendoring Go dependencies
+}
+
+CHANNEL_NAME="mychannel"
+CC_RUNTIME_LANGUAGE="golang"
+VERSION="1"
+CC_SRC_PATH="./artifacts/src/github.com/fabcar/go"
+CC_NAME="fabcar"
+
+packageChaincode() {
+    rm -rf ${CC_NAME}.tar.gz
+    setGlobalsForProducer1Peer0
+    peer lifecycle chaincode package ${CC_NAME}.tar.gz \
+        --path ${CC_SRC_PATH} --lang ${CC_RUNTIME_LANGUAGE} \
+        --label ${CC_NAME}_${VERSION}
+    echo "===================== Chaincode is packaged on peer0.producer1 ===================== "
+}
+
+installChaincode() {
+    setGlobalsForProducer1Peer0
+    peer lifecycle chaincode install ${CC_NAME}.tar.gz
+    echo "===================== Chaincode is installed on peer0.producer1 ===================== "
+
+    # setGlobalsForProducer1Peer1
+    # peer lifecycle chaincode install ${CC_NAME}.tar.gz
+    # echo "===================== Chaincode is installed on peer1.producer1 ===================== "
+
+    setGlobalsForProducer2Peer0
+    peer lifecycle chaincode install ${CC_NAME}.tar.gz
+    echo "===================== Chaincode is installed on peer0.producer2 ===================== "
+
+    # setGlobalsForProducer2Peer1
+    # peer lifecycle chaincode install ${CC_NAME}.tar.gz
+    # echo "===================== Chaincode is installed on peer1.producer2 ===================== "
+}
+
+queryInstalled() {
+    setGlobalsForProducer1Peer0
+    peer lifecycle chaincode queryinstalled >&log.txt
+    cat log.txt
+    PACKAGE_ID=$(sed -n "/${CC_NAME}_${VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
+    echo PackageID is ${PACKAGE_ID}
+    echo "===================== Query installed successful on peer0.producer1 on channel ===================== "
+}
+
+approveForMyProducer11() {
+    setGlobalsForProducer1Peer0
+    peer lifecycle chaincode approveformyorg -o localhost:7050 \
+        --ordererTLSHostnameOverride orderer.example.com --tls \
+        --collections-config $PRIVATE_DATA_CONFIG \
+        --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
+        --init-required --package-id ${PACKAGE_ID} \
+        --sequence ${VERSION}
+
+    echo "===================== chaincode approved from Producer1 ===================== "
+}
+
+getBlock() {
+    setGlobalsForProducer1Peer0
+    # peer channel fetch 10 -c mychannel -o localhost:7050 \
+    #     --ordererTLSHostnameOverride orderer.example.com --tls \
+    #     --cafile $ORDERER_CA
+
+    peer channel getinfo  -c mychannel -o localhost:7050 \
+        --ordererTLSHostnameOverride orderer.example.com --tls \
+        --cafile $ORDERER_CA
+}
+
+checkCommitReadyness() {
+    setGlobalsForProducer1Peer0
+    peer lifecycle chaincode checkcommitreadiness \
+        --collections-config $PRIVATE_DATA_CONFIG \
+        --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
+        --sequence ${VERSION} --output json --init-required
+    echo "===================== checking commit readyness from Producer1 ===================== "
+}
+
+approveForMyProducer2() {
+    setGlobalsForProducer2Peer0
+    peer lifecycle chaincode approveformyorg -o localhost:7050 \
+        --ordererTLSHostnameOverride orderer.example.com --tls $CORE_PEER_TLS_ENABLED \
+        --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} \
+        --collections-config $PRIVATE_DATA_CONFIG \
+        --version ${VERSION} --init-required --package-id ${PACKAGE_ID} \
+        --sequence ${VERSION} \
+
+    echo "===================== chaincode approved from Producer2 ===================== "
+}
+
+commitChaincodeDefinition() {
+    setGlobalsForProducer1Peer0
+    peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com \
+        --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
+        --channelID $CHANNEL_NAME --name ${CC_NAME} \
+        --collections-config $PRIVATE_DATA_CONFIG \
+        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_PRODUCER1_CA \
+        --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_PRODUCER2_CA \
+        --version ${VERSION} --sequence ${VERSION} --init-required
+}
+
+queryCommitted() {
+    setGlobalsForProducer1Peer0
+    peer lifecycle chaincode querycommitted --channelID $CHANNEL_NAME --name ${CC_NAME}
+}
+
+chaincodeInvokeInit() {
+    setGlobalsForProducer1Peer0
+    peer chaincode invoke -o localhost:7050 \
+        --ordererTLSHostnameOverride orderer.example.com \
+        --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
+        -C $CHANNEL_NAME -n ${CC_NAME} \
+        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_PRODUCER1_CA \
+        --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_PRODUCER2_CA \
+        --isInit -c '{"Args":[]}'
+}
+
+chaincodeInvoke() {
+    ## Init ledger
+    setGlobalsForProducer1Peer0
+    peer chaincode invoke -o localhost:7050 \
+        --ordererTLSHostnameOverride orderer.example.com \
+        --tls $CORE_PEER_TLS_ENABLED \
+        --cafile $ORDERER_CA \
+        -C $CHANNEL_NAME -n ${CC_NAME} \
+        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_PRODUCER1_CA \
+        --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_PRODUCER2_CA \
+        -c '{"function": "initLedger","Args":[]}'
+
+    ## Add private data
+    export CAR=$(echo -n "{\"key\":\"1111\", \"make\":\"Tesla\",\"model\":\"Tesla A1\",\"color\":\"White\",\"owner\":\"niki\",\"price\":\"10000\"}" | base64 | tr -d \\n)
+    peer chaincode invoke -o localhost:7050 \
+        --ordererTLSHostnameOverride orderer.example.com \
+        --tls $CORE_PEER_TLS_ENABLED \
+        --cafile $ORDERER_CA \
+        -C $CHANNEL_NAME -n ${CC_NAME} \
+        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_PRODUCER1_CA \
+        --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_PRODUCER2_CA \
+        -c '{"function": "createPrivateCar", "Args":[]}' \
+        --transient "{\"car\":\"$CAR\"}"
+}
+
+chaincodeQuery() {
+    setGlobalsForPeer0Org2
+
+    # Query all cars
+    # peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"Args":["queryAllCars"]}'
+
+    # Query Car by Id
+    peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"function": "queryCar","Args":["CAR0"]}'
+    #'{"Args":["GetSampleData","Key1"]}'
+
+    # Query Private Car by Id
+    # peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"function": "readPrivateCar","Args":["1111"]}'
+    # peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"function": "readCarPrivateDetails","Args":["1111"]}'
+}
+
+# Run this function if you add any new dependency in chaincode
+presetup
+
+packageChaincode
+installChaincode
+queryInstalled
+approveForMyOrg1
+checkCommitReadyness
+approveForMyOrg2
+checkCommitReadyness
+commitChaincodeDefinition
+queryCommitted
+chaincodeInvokeInit
+sleep 5
+chaincodeInvoke
+sleep 3
+chaincodeQuery
