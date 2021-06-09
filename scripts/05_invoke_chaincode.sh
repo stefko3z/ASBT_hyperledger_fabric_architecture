34880324982ff9b54018b0c5e4fb1c258296d5ef
Stefan Vladov <svladov.dev@gmail.com>
2021-06-09 22:59:32 +0200

    uncomment invoke

--- scripts/05_invoke_chaincode.sh
+++ scripts/05_invoke_chaincode.sh
@@ -7,17 +7,17 @@
 produceVaccine() {
     setGlobalsForProducer1Peer0
     setGlobalsForVaccineChaincode
-    # peer chaincode invoke -o localhost:7050 \
-    #     --ordererTLSHostnameOverride orderer.example.com \
-    #     --tls $CORE_PEER_TLS_ENABLED \
-    #     --cafile $ORDERER_CA \
-    #     -C $CHANNEL_NAME -n ${CC_NAME} \
-    #     --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_PRODUCER1_CA \
-    #     --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_PRODUCER2_CA \
-    #     --peerAddresses localhost:11051 --tlsRootCertFiles $PEER0_HOSPITAL1_CA \
-    #     --peerAddresses localhost:12051 --tlsRootCertFiles $PEER0_HOSPITAL2_CA \
-    #     --waitForEvent \
-    #     -c '{"function":"produce","Args":["Pfizer", "20"]}'
+    peer chaincode invoke -o localhost:7050 \
+        --ordererTLSHostnameOverride orderer.example.com \
+        --tls $CORE_PEER_TLS_ENABLED \
+        --cafile $ORDERER_CA \
+        -C $CHANNEL_NAME -n ${CC_NAME} \
+        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_PRODUCER1_CA \
+        --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_PRODUCER2_CA \
+        --peerAddresses localhost:11051 --tlsRootCertFiles $PEER0_HOSPITAL1_CA \
+        --peerAddresses localhost:12051 --tlsRootCertFiles $PEER0_HOSPITAL2_CA \
+        --waitForEvent \
+        -c '{"function":"produce","Args":["Pfizer", "20"]}'
 
     peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"function": "getVaccine","Args":["v1010"]}'
 
